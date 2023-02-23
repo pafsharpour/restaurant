eecb9a1a0b84fad9cbe82ed2c315a6529ac5ba49
pariya.af <afsharpour.pariya@gmail.com>
2023-02-23 12:45:44 +0330

    add sidebar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -10,6 +10,9 @@ use yii\bootstrap5\Html;
 use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
 
+use kartik\sidenav\SideNav;
+use yii\helper\url;
+
 AppAsset::register($this);
 
 //sesion for error or success message
@@ -21,6 +24,48 @@ if ($session->isActive)
 <h2><?php echo $session->get('result'); ?></h2>
 <?php
 $session->set('result', '');
+
+
+/** @var yii\web\View $this */
+$this->title = 'My Yii Application';
+     ?>
+     <div class='col-2 m-5'>
+     <?php
+echo SideNav::widget([
+	'type' => SideNav::TYPE_DEFAULT,
+	'heading' => 'Options',
+	'items' => [
+		[
+			'url' => '/customer',
+			'label' => 'customer',
+			'icon' => 'home'
+		],
+        [
+			'url' => '/branches',
+			'label' => 'branches',
+			'icon' => 'home'
+		],
+        [
+			'url' => '/orders',
+			'label' => 'orders',
+			'icon' => 'home'
+		],
+		[
+			'label' => 'FOODS',
+			'icon' => 'question-sign',
+			'items' => [
+				['label' => 'foods', 'icon'=>'info-sign', 'url'=>'/foods'],
+				['label' => 'food type', 'icon'=>'phone', 'url'=>'foodtype'],
+			],
+		],
+	],
+]);
+
+?>
+</div>
+<div class='col-10 m-5'>
+
+<?php
 
 
 $this->registerCsrfMetaTags();
@@ -40,34 +85,8 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <body class="d-flex flex-column h-100">
 <?php $this->beginBody() ?>
 
-<header id="header">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest
-                ? ['label' => 'Login', 'url' => ['/site/login']]
-                : '<li class="nav-item">'
-                    . Html::beginForm(['/site/logout'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'nav-link btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
-        ]
-    ]);
-    NavBar::end();
-    ?>
-</header>
+
+
 
 <main id="main" class="flex-shrink-0" role="main">
     <div class="container">
@@ -79,14 +98,8 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     </div>
 </main>
 
-<footer id="footer" class="mt-auto py-3 bg-light">
-    <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
-        </div>
-    </div>
-</footer>
+
+
 
 <?php $this->endBody() ?>
 </body>
